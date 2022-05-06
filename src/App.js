f2f45514f1cfff807dcdfc5d13bac3f3f6d51364
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2022-05-06 12:34:54 +0100

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
+    <div className="container">
     <div className="App">
       <header className="App-header"> Hello
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        Coded by Gina-Camelia Hodorogea
+      </footer>
+    </div>
     </div>
   );
 }
